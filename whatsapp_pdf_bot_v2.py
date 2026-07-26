b1aa06117a7a57ee5eaf02c33da11f44d71ea546
mohamdmut002 <mohamdmut002@gmail.com>
2026-07-27 03:04:51 +0400

    Update whatsapp_pdf_bot_v2.py

--- whatsapp_pdf_bot_v2.py
+++ whatsapp_pdf_bot_v2.py
@@ -279,8 +279,8 @@ def serve_output(filename):
     return send_from_directory(OUTPUT_DIR, filename)
 
 
-  init_db()
+init_db()
 
-   if __name__ == "__main__":
-       port = int(os.environ.get("PORT", 5000))
-       app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
